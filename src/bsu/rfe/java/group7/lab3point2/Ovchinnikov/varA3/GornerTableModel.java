9de131a63e459838645c14ef1aeeaee72c183f52
OvchinnikovVladislav <152195166+OvchinnikovVladislav@users.noreply.github.com>
2023-12-19 17:12:48 +0300

    lab3point2

---
+++ src/bsu/rfe/java/group7/lab3point2/Ovchinnikov/varA3/GornerTableModel.java
@@ -0,0 +1,76 @@
+package bsu.rfe.java.group7.lab3point2.Ovchinnikov.varA3;
+
+import javax.swing.table.AbstractTableModel;
+@SuppressWarnings("serial")
+public class GornerTableModel extends AbstractTableModel {
+    private Double[] coefficients;
+    private Double from;
+    private Double to;
+    private Double step;
+    public GornerTableModel(Double from, Double to, Double step,
+                            Double[] coefficients) {
+        this.from = from;
+        this.to = to;
+        this.step = step;
+        this.coefficients = coefficients;
+    }
+    public Double getFrom() {
+        return from;
+    }
+    public Double getTo() {
+        return to;
+    }
+    public Double getStep() {
+        return step;
+    }
+    public int getColumnCount() {
+// В данной модели два столбца
+        return 3;
+    }
+    public int getRowCount() {
+// Вычислить количество точек между началом и концом отрезка
+// исходя из шага табулирования
+        return new Double(Math.ceil((to - from) / step)).intValue() + 1;
+    }
+    public Object getValueAt(int row, int col) {
+// Вычислить значение X как НАЧАЛО_ОТРЕЗКА + ШАГ*НОМЕР_СТРОКИ
+        Double result = 0.0;
+        double x = from + step*row;
+        if (col==0) {
+// Если запрашивается значение 1-го столбца, то это X
+            return x;
+        } else if (col == 1) {
+// Если запрашивается значение 2-го столбца, то это значение
+// многочлена
+            for (int i = 0; i <= coefficients.length - 1; i++) {
+                result = result * x + coefficients[i];
+            }
+            return result;
+        }
+        else {
+            for (int i = 0; i <= coefficients.length - 1; i++) {
+                result = result * x + coefficients[i];
+            }
+            return result % 1 == 0;
+        }
+    }
+    public String getColumnName(int col) {
+        switch (col) {
+            case 0:
+// Название 1-го столбца
+                return "Значение X";
+            case 1:
+// Название 2-го столбца
+                return "Значение многочлена";
+            default:
+                return "Точное значение?";
+        }
+    }
+    public Class<?> getColumnClass(int col) {
+        if (col == 2) {
+            return Boolean.class;
+        } else {
+            return Double.class;
+        }
+    }
+}
